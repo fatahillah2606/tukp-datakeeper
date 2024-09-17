0deb5be533d208332ab2afcb413e750fa4a544e8
fatahillah2606 <muhamad.fatahillah@raharja.info>
2024-09-18 01:09:41 +0700

    Reconfigure database structure

--- tukp_datakeeper.sql
+++ tukp_datakeeper.sql
@@ -2,10 +2,10 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Sep 09, 2024 at 05:49 PM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
+-- Host: localhost
+-- Generation Time: Sep 17, 2024 at 06:03 PM
+-- Server version: 8.0.39
+-- PHP Version: 8.3.9
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -31,15 +31,15 @@ USE `tukp_datakeeper`;
 
 DROP TABLE IF EXISTS `data_barang_eksternal`;
 CREATE TABLE IF NOT EXISTS `data_barang_eksternal` (
-  `id` int(255) NOT NULL AUTO_INCREMENT,
+  `id` int NOT NULL AUTO_INCREMENT,
   `tanggal` date NOT NULL,
-  `nama_driver` varchar(255) NOT NULL,
-  `nama_suplier` varchar(255) NOT NULL,
-  `keperluan` varchar(255) NOT NULL,
-  `nama_jumlah_barang` varchar(255) NOT NULL,
-  `no_kendaraan` varchar(255) NOT NULL,
+  `nama_driver` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `nama_suplier` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `keperluan` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `nama_jumlah_barang` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `no_kendaraan` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
   `jam_kedatangan` time NOT NULL,
-  `keterangan` varchar(255) NOT NULL,
+  `keterangan` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -51,11 +51,11 @@ CREATE TABLE IF NOT EXISTS `data_barang_eksternal` (
 
 DROP TABLE IF EXISTS `data_barang_internal`;
 CREATE TABLE IF NOT EXISTS `data_barang_internal` (
-  `id` int(255) NOT NULL AUTO_INCREMENT,
-  `nama_pembawa` varchar(255) NOT NULL,
-  `nama_jumlah_barang` varchar(255) NOT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nama_pembawa` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `nama_jumlah_barang` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
   `tanggal` date NOT NULL,
-  `keterangan` varchar(255) NOT NULL,
+  `keterangan` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -67,13 +67,13 @@ CREATE TABLE IF NOT EXISTS `data_barang_internal` (
 
 DROP TABLE IF EXISTS `data_mobil`;
 CREATE TABLE IF NOT EXISTS `data_mobil` (
-  `id` int(255) NOT NULL AUTO_INCREMENT,
-  `nama_driver` varchar(255) NOT NULL,
-  `merek_kendaraan` varchar(255) NOT NULL,
-  `km_awal` int(255) NOT NULL,
-  `km_akhir` int(255) NOT NULL,
-  `tujuan` varchar(255) NOT NULL,
-  `keperluan` varchar(255) NOT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nama_driver` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `merek_kendaraan` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `km_awal` int NOT NULL,
+  `km_akhir` int NOT NULL,
+  `tujuan` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `keperluan` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -85,11 +85,11 @@ CREATE TABLE IF NOT EXISTS `data_mobil` (
 
 DROP TABLE IF EXISTS `data_pengunjung`;
 CREATE TABLE IF NOT EXISTS `data_pengunjung` (
-  `id` int(255) NOT NULL AUTO_INCREMENT,
-  `nama_pengunjung` varchar(255) NOT NULL,
-  `nama_perusahaan` varchar(255) NOT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nama_pengunjung` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `nama_perusahaan` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
   `tanggal` date NOT NULL,
-  `no_telpon` varchar(13) NOT NULL,
+  `no_telpon` varchar(13) COLLATE utf8mb4_general_ci NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -101,19 +101,26 @@ CREATE TABLE IF NOT EXISTS `data_pengunjung` (
 
 DROP TABLE IF EXISTS `pengguna`;
 CREATE TABLE IF NOT EXISTS `pengguna` (
-  `id` int(255) NOT NULL AUTO_INCREMENT,
-  `id_user` int(255) DEFAULT NULL,
-  `email_user` varchar(255) DEFAULT NULL,
-  `nama_user` varchar(255) NOT NULL,
-  `role` varchar(255) NOT NULL,
-  `password` longtext NOT NULL,
-  `token_login` longtext NOT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
+  `id_user` int DEFAULT NULL,
+  `email_user` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `nama_user` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `role` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `token_login` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
   PRIMARY KEY (`id`),
+  UNIQUE KEY `token_login` (`token_login`),
   UNIQUE KEY `id-user` (`id_user`),
   UNIQUE KEY `email-user` (`email_user`),
-  UNIQUE KEY `id_user` (`id_user`,`email_user`,`token_login`) USING HASH,
-  UNIQUE KEY `token_login` (`token_login`) USING HASH
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  UNIQUE KEY `id_user` (`id_user`,`email_user`,`token_login`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `pengguna`
+--
+
+INSERT INTO `pengguna` (`id`, `id_user`, `email_user`, `nama_user`, `role`, `password`, `token_login`) VALUES
+(2, NULL, 'andika@admin', 'Andika', 'Admin', '$2y$10$e5cMNe7GUr6AlWQ1zkqaXe8SKlqm1g/HlrucDpfMN.Gwt0TxKTBwG', NULL);
 
 -- --------------------------------------------------------
 
@@ -123,8 +130,8 @@ CREATE TABLE IF NOT EXISTS `pengguna` (
 
 DROP TABLE IF EXISTS `reset_sandi`;
 CREATE TABLE IF NOT EXISTS `reset_sandi` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `cari_pengguna` varchar(255) NOT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
+  `cari_pengguna` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
   `dibaca` tinyint(1) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
