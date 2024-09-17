70622a0e471967cbe376680cf3a5062d0ed0d314
codoters <andikakurniawan.ak17@gmail.com>
2024-09-17 21:39:36 +0700

    proses-login.php fix

--- functions/proses-login.php
+++ functions/proses-login.php
@@ -7,7 +7,7 @@ if (isset($_POST["LoginUser"])) {
     $userid = htmlspecialchars($_POST["userid"]);
     $password = htmlspecialchars($_POST["password"]);
    
-    $sql = "SELECT 'id_user', 'nama_user', 'role', 'password', 'token_login' FROM pengguna WHERE 'user_id' = ?";
+    $sql = "SELECT id_user, nama_user, role, password, token_login FROM pengguna WHERE id_user = ?";
     $stmt = $conn->prepare($sql);
     $stmt->bind_param("i", $userid);
     $stmt->execute();
@@ -17,19 +17,21 @@ if (isset($_POST["LoginUser"])) {
     if ($password && password_verify($password, $Password)) {
         $_SESSION["user_id"] = $UserId;
         $_SESSION["nama_user"] = $NamaUser;
-
+        $stmt->close();
+        echo $UserId;
 
         $token = bin2hex(random_bytes(16));
         $hashedToken = password_hash($token,PASSWORD_BCRYPT);
+        echo $hashedToken;
 
         $sql = "UPDATE pengguna SET token_login = ? WHERE id_user = ?";
         $stmt = $conn->prepare($sql);
-        $stmt->bind_param("si", $hashedToken, $userid);
+        $stmt->bind_param("si", $hashedToken, $UserId);
+        $stmt->execute();
 
         setcookie("ingat_saya", $UserId.":".$token, time() + (30*24*60*60),"/");
 
-    }
-
+    } else {echo "error Tolol";}
 }else{
 echo "error Tolol";
 }
