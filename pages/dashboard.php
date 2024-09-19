ea7f32da8657a000a58d08f2bc4ee16d7b45f2a6
Muhaimin Al Aziz Hasibuan <muhaiminalaziz06@gmail.com>
2024-09-19 21:04:44 +0700

    perubahan navbar

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -2,9 +2,15 @@
 <html lang="en">
   <head>
     <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1.0"
+    />
     <title>Document</title>
-    <link rel="stylesheet" href="../assets/css/style.css" />
+    <link
+      rel="stylesheet"
+      href="../assets/css/style.css"
+    />
     <link
       rel="stylesheet"
       href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
@@ -12,6 +18,10 @@
     <link
       href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css"
       rel="stylesheet"
+    />
+    <link
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
     />
   </head>
   <body>
@@ -27,13 +37,18 @@
         <!-- Sidebar -->
         <nav class="col-md-2 d-none d-md-block sidebar">
           <div class="position-sticky">
-            <a href="#" class="navbar-brand mb-3 text-center">
-              <img src="../assets/images/logo.svg" alt="Logo" />
-              <span class="text-white">TUKP Data Keeper</span>
+            <a
+              href="#"
+              class="navbar-brand mb-1"
+            >
+              <span class="material-symbols-outlined"> menu </span>
             </a>
             <ul class="nav flex-column">
               <li class="nav-item">
-                <a class="nav-link active" href="#">
+                <a
+                  class="nav-link active"
+                  href="#"
+                >
                   <i class="bi bi-speedometer2"></i> Dashboard
                 </a>
               </li>
@@ -45,64 +60,95 @@
                   role="button"
                 >
                   <!-- Pencatatan -->
-                  <i class="bi bi-pencil-square"></i> Pencatatan
+                  <i class="bi bi-pencil-square"></i> Pencatatan <span class="material-symbols-outlined">arrow_drop_down_circle</span>
                 </a>
-                <div class="collapse" id="collapsePencatatan">
+                <div
+                  class="collapse"
+                  id="collapsePencatatan"
+                >
                   <ul class="nav flex-column ms-3">
                     <li class="nav-item">
-                      <a class="nav-link" href="#"
+                      <a
+                        class="nav-link"
+                        href="#"
                         ><i class="bi bi-person-check"></i> Catat Pengunjung</a
                       >
                     </li>
                     <li class="nav-item">
-                      <a class="nav-link" href="#"
+                      <a
+                        class="nav-link"
+                        href="#"
                         ><i class="bi bi-box"></i> Catat Barang</a
                       >
                     </li>
                     <li class="nav-item">
-                      <a class="nav-link" href="#"
+                      <a
+                        class="nav-link"
+                        href="#"
                         ><i class="bi bi-truck"></i> Catat Mobil</a
                       >
                     </li>
                   </ul>
                 </div>
               </li>
+              <!-- Lihat data -->
+
               <li class="nav-item">
-                <a class="nav-link" href="#"
-                  ><i class="bi bi-eye"></i> Lihat Data
+                <a
+                  class="nav-link"
+                  data-bs-toggle="collapse"
+                  href="#collapseLihatData"
+                  role="button"
+                >
+                  <i class="bi bi-eye"></i> Lihat Data <span class="material-symbols-outlined">arrow_drop_down_circle</span>
                 </a>
-                <div class="collapse" id="collapseLihatData">
+                <div
+                  class="collapse"
+                  id="collapseLihatData"
+                >
                   <ul class="nav flex-column ms-3">
                     <li class="nav-item">
-                      <a class="nav-link" href="#"
-                        ><i class="bi bi-person-check"></i> Data Pengunjung</a
+                      <a
+                        class="nav-link"
+                        href="#"
+                        ><i class="bi bi-person-check"></i> Lihat Pengunjung</a
                       >
                     </li>
                     <li class="nav-item">
-                      <a class="nav-link" href="#"
-                        ><i class="bi bi-box"></i> Data Barang</a
+                      <a
+                        class="nav-link"
+                        href="#"
+                        ><i class="bi bi-box"></i> Lihat Barang</a
                       >
                     </li>
                     <li class="nav-item">
-                      <a class="nav-link" href="#"
-                        ><i class="bi bi-truck"></i> Data Mobil</a
+                      <a
+                        class="nav-link"
+                        href="#"
+                        ><i class="bi bi-truck"></i> Lihat Mobil</a
                       >
                     </li>
                   </ul>
                 </div>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="#"
+                <a
+                  class="nav-link"
+                  href="#"
                   ><i class="bi bi-person-plus"></i> Tambah Pengguna</a
                 >
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="#"
+                <a
+                  class="nav-link"
+                  href="#"
                   ><i class="bi bi-bell"></i> Notifikasi</a
                 >
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="#"
+                <a
+                  class="nav-link"
+                  href="#"
                   ><i class="bi bi-box-arrow-right"></i> Keluar</a
                 >
               </li>
@@ -111,9 +157,20 @@
         </nav>
 
         <!-- Main content -->
-        <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4 content">
+        <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4 content position-relative">
           <!-- Header -->
-          <div class="header">
+          <div
+            class="d-block position-absolute top-0 start-0 w-100"
+            style="background-color: #158c4c"
+          >
+            <img
+              src="../assets/images/logo.svg"
+              alt="Logo"
+              class="bg-light rounded-circle p-1"
+            />
+            <span class="text-white">TUKP Data Keeper</span>
+          </div>
+          <div class="header mt-5">
             <h1><span class="text-primary">Selamat Datang,</span> Al Aziz</h1>
             <p class="lead">Mau Apa Kamu Hari Ini?</p>
           </div>
