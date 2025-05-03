b3e0ad6024fb7e20c669e1e276b3c3a9d3443f24
codoters <andikakurniawan.ak17@gmail.com>
2025-05-03 21:44:30 +0700

    Create the login page
    - Membuat halaman login
    - Membuat isi halaman login

--- index.php
+++ index.php
@@ -11,15 +11,67 @@
         <link rel="stylesheet" href="assets/style/login.css" />
     </head>
     <body>
-        <div class="card" style="width: 18rem">
-            <img src="..." class="card-img-top" alt="..." />
+        <div class="card">
+            <div class="card-header d-flex align-items-center">
+                <img
+                    src="assets/images/logo.svg"
+                    alt="Logo Taland Utama Karisma Perkasa"
+                    class="me-2"
+                />
+                <p class="h2">PT Taland Utama Karisma Perkasa</p>
+            </div>
             <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">
-                    Some quick example text to build on the card title and make
-                    up the bulk of the card's content.
-                </p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
+                <form action="" method="post">
+                    <p class="h3">Login</p>
+                    <select
+                        class="form-select mb-3"
+                        aria-label="Jenis Pengguna"
+                        name="pengguna"
+                    >
+                        <option value="admin">Admin</option>
+                        <option value="security" selected>Security</option>
+                        <option value="tamu">Tamu</option>
+                    </select>
+                    <div class="mb-3">
+                        <label for="userid" class="form-label"
+                            >Id Pengguna</label
+                        >
+                        <input
+                            type="number"
+                            class="form-control"
+                            id="userid"
+                            placeholder="123456"
+                        />
+                    </div>
+                    <div class="mb-3">
+                        <label for="userpassword" class="form-label"
+                            >Sandi Pengguna</label
+                        >
+                        <input
+                            type="password"
+                            class="form-control"
+                            id="userpassword"
+                            placeholder=""
+                        />
+                    </div>
+                    <div class="form-check mb-3">
+                        <input
+                            class="form-check-input"
+                            type="checkbox"
+                            value=""
+                            id="showpw"
+                        />
+                        <label class="form-check-label" for="showpw">
+                            Tampilkan Sandi
+                        </label>
+                    </div>
+                    <div class="btn-grup">
+                        <a href="#" class="btn btn-secondary"> Lupa Sandi </a>
+                        <button type="submit" class="btn btn-success">
+                            Submit
+                        </button>
+                    </div>
+                </form>
             </div>
         </div>
         <script src="assets/bootstrap-5.3.5-dist/js/bootstrap.bundle.min.js"></script>
