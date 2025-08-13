c04f43d829097db28dd9191da608d7eff63d7903
codoters <andikakurniawan.ak17@gmail.com>
2025-08-13 20:55:00 +0700

    update kelola data
    - update minor pada bagian kelola data .php

--- backend/kelola_data.php
+++ backend/kelola_data.php
@@ -9,7 +9,7 @@ require "generate_api.php";
 
 if ($_SERVER["REQUEST_METHOD"] === "GET") {
     
-    // Request data barang
+    // Request data barang internal
     if (isset($_GET["data_internal"])) {
 
         $query = null;
@@ -17,13 +17,43 @@ if ($_SERVER["REQUEST_METHOD"] === "GET") {
 
         // Jika ada limit
         if (isset($_GET["limit"])) {
-            $query = "SELECT * FROM data_barang_internal ORDER BY id DESC LIMIT :batasan";
+            $query = "SELECT * FROM data_barang_internal ORDER BY id_barang_internal DESC LIMIT :batasan";
             $limit = htmlspecialchars($_GET["limit"]);
 
             $stmt = $pdo->prepare($query);
             $stmt->execute(["batasan" => $limit]);
         } else {
-            $query = "SELECT * FROM data_barang_internal ORDER BY id DESC";
+            $query = "SELECT * FROM data_barang_internal ORDER BY id_barang_internal DESC";
+            $stmt = $pdo->prepare($query);
+            $stmt->execute();
+        }
+
+        // Fetch hasilnya
+        $hasil = $stmt->fetchAll();
+
+        // cek hasil
+        if (empty($hasil)) {
+            echo json_encode(generateAPI("failed", 404, "Data tidak tersedia", []), JSON_PRETTY_PRINT);
+        } else {
+            // Tampilkan hasil
+            echo json_encode(generateAPI("success", 200, "Data tersedia", $hasil), JSON_PRETTY_PRINT);
+        }
+    }
+    // Request data barang eksternal
+    if (isset($_GET["data_eksternal"])) {
+
+        $query = null;
+        $stmt = null;
+
+        // Jika ada limit
+        if (isset($_GET["limit"])) {
+            $query = "SELECT * FROM data_barang_eksternal ORDER BY id_barang_eksternal DESC LIMIT :batasan";
+            $limit = htmlspecialchars($_GET["limit"]);
+
+            $stmt = $pdo->prepare($query);
+            $stmt->execute(["batasan" => $limit]);
+        } else {
+            $query = "SELECT * FROM data_barang_eksternal ORDER BY id_barang_eksternal DESC";
             $stmt = $pdo->prepare($query);
             $stmt->execute();
         }
